14fbd803f401af54711e14138ef3cb6bffc266f6
noufal <noufal@hashcodeit.com>
2023-12-19 22:02:26 +0400

    update zip

--- hc_timesheet_billable/models/hr.py
+++ hc_timesheet_billable/models/hr.py
@@ -11,12 +11,12 @@ class Timesheet(models.Model):
     _inherit = "account.analytic.line"
 
     ratio_factor = fields.Float(string='Ratio Factor', related='employee_id.ratio_factor')
-    billable = fields.Float(string='Billable', compute='_compute_hc_billable')
+    billable_value = fields.Float(string='Billable Total', compute='_compute_hc_billable')
 
     @api.depends('ratio_factor', 'unit_amount')
     def _compute_hc_billable(self):
         for rec in self:
             if rec.ratio_factor and rec.unit_amount:
-                rec.billable = rec.ratio_factor * rec.unit_amount
+                rec.billable_value = rec.ratio_factor * rec.unit_amount
             else:
-                rec.billable = 0
+                rec.billable_value = 0
